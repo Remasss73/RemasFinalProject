304e339b8e5d1bc8ae05ea799aa3e4adeb264c57
class01 <remasali476@gmail.com>
2025-11-06 09:36:30 +0200

    gggg

--- app/src/main/java/remas/example/remasfinalproject/HomeScreen.java
+++ app/src/main/java/remas/example/remasfinalproject/HomeScreen.java
@@ -1,6 +1,7 @@
 package remas.example.remasfinalproject;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HomeScreen extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity
+{
+    private TextView tv_Rumi;
+    private TextView tv_Filter;
+    private TextView tv_Rent;
+    private TextView tv_City1;
+    private TextView tv_Rent1;
+    private TextView tv_City2;
+    private TextView tv_Home;
+    private TextView tv_Matches;
+    private TextView tv_Add;
+    private TextView tv_Profile;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        tv_Rumi= findViewById(R.id.tvRumi);
+        tv_Filter = findViewById(R.id.tvFilter);
+        tv_Rent = findViewById(R.id.tvRent);
+        tv_City1 = findViewById(R.id.tvCity1);
+        tv_Rent1 = findViewById(R.id.tvRent1);
+        tv_City2 = findViewById(R.id.tvCity2);
+        tv_Home = findViewById(R.id.tvHome);
+        tv_Matches = findViewById(R.id.tvMatches);
+        tv_Add = findViewById(R.id.tvAdd);
+        tv_Profile = findViewById(R.id.tvProfile);
+
+
+        
+
+        };
     }
-}
